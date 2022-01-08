251d32468421584c0cadcc9414164e3952f1a6b4
gimdongwon <ehddnjs8989@gmail.com>
2022-01-09 02:47:31 +0900

    myAccount 조회 완료

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const axios = require('axios');
+const { v4: uuidv4 } = require('uuid');
+const sign = require('jsonwebtoken').sign;
+
 const router = express.Router();
+require('dotenv').config();
+
+const access_key = process.env.UPBIT_OPEN_API_ACCESS_KEY;
+const secret_key = process.env.UPBIT_OPEN_API_SECRET_KEY;
+
+const payload = {
+  access_key: access_key,
+  nonce: uuidv4(),
+};
+
+const token = sign(payload, secret_key);
+const authorizationToken = `Bearer ${token}`;
 
 const getMarketAll = () => {
   const options = {
@@ -38,7 +53,25 @@ const getCoinInfos = (name) => {
     });
 };
 
+const getMyAccountInfos = () => {
+  const options = {
+    method: 'GET',
+    url: 'https://api.upbit.com/v1/accounts',
+    headers: { Authorization: authorizationToken },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+};
+
 router.get('/', (req, res) => {
+  getMyAccountInfos();
   res.send('');
 });
 
